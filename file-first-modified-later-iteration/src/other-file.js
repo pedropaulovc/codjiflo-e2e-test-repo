9e339d274b74515b5a0d6de1abcfe43d9b022537
Pedro Paulo Vezza Campos <pedro@vezza.com.br>
2026-01-17 12:55:45 -0800

    Add test data for file-first-modified-later-iteration test
    This sets up the base state for testing that files first modified
    in iteration 2 are correctly marked as Modified (not Added).
    
    See file-first-modified-later-iteration/README.md for details.

---
+++ file-first-modified-later-iteration/src/other-file.js
@@ -0,0 +1,8 @@
+// This file will be modified in iteration 1
+// target-file.yml will NOT be touched in iteration 1
+
+function helper() {
+  return 'iteration 1 base version';
+}
+
+module.exports = { helper };
